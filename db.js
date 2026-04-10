b964841970f50411d3b45f713ffd206870cf8383
villo33 <romeroovidio02@yahoo.com>
2026-04-10 15:28:59 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1003645363",
-    database: "crochet"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "1003645363",
+    database: process.env.DB_NAME || "crochet",
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
-    if(err) console.log(err);
-    else console.log("BD conectada");
+    if(err){
+        console.log("❌ Error conexión:", err);
+    }else{
+        console.log("✅ Base de datos conectada");
+    }
 });
 
 module.exports = db;
